280d425c3dfb502cd42d1c61b1dfb79326460c4d
Himanshu <technophilic000@gmail.com>
2022-07-26 09:40:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [temp,setTemp] = useState(0);
+  const [color,setColor] = useState("DodgerBlue");
+
+const Increment =() =>{
+  const newTemp = temp +1;
+  if(newTemp > 20){
+    setColor("orange")
+  }
+   setTemp(temp+1);
+}
+
+const Decrement = ()=>{
+  const newTemp = temp -1;
+  if(newTemp < 20){
+     setColor("DodgerBlue")
+  }
+  return setTemp(temp-1);
+      
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='App'>
+     <h1>Hello</h1>
+     <div className='circle' style={{background: color}}><h1>{temp} 'C</h1></div>
+     <button className='increment' onClick={Increment}>+</button>
+     <button className='decrement' onClick={Decrement}>-</button>
+
+      </div>
     </div>
   );
 }
